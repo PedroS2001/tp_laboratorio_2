b33b7f92990da5b8784e22b2ff618ce9d860029f
Pedro <pedroseneriz01@gmail.com>
2020-09-12 18:43:59 -0300

    tp 1 realizado

---
+++ TrabajoPractico1/Entidades/Calculadora.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Valida que el operador sea +,-,/,* o sino devuelve +
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        private static string ValidarOperador(char operador)
+        {
+            string retorno;
+
+            switch(operador)
+            {
+                case '-':
+                    retorno = "-";
+                    break;
+                case '*':
+                    retorno = "*";
+                    break;
+                case '/':
+                    retorno = "/";
+                    break;
+                default:
+                    retorno = "+";
+                    break;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// llama a la anterior para validar el operador y realiza la operacion
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double retorno = 0;
+
+            char aValidar;
+            char.TryParse(operador, out aValidar);
+            operador = Calculadora.ValidarOperador(aValidar);
+            
+
+            switch (operador)
+            {
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        retorno = num1 / num2;
+                    break;
+                default:
+                    retorno = num1 + num2;
+                    break;
+            }
+            return retorno;
+        }
+
+    }
+}
---
+++ TrabajoPractico1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,92 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            //agrego esto para que el primer operador sea en blanco y no nulo
+            this.cmbOperador.Items.Insert(0, "");
+            this.cmbOperador.SelectedIndex = 0;
+        }
+
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado;
+            string operador;
+            operador = this.cmbOperador.SelectedItem.ToString();
+            resultado = FormCalculadora.Operar(this.txtNumero1.Text, this.txtNumero2.Text, operador);
+
+            this.lblResultado.Text = resultado.ToString();
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Numero num1 = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+            double retorno = Calculadora.Operar(num1,num2,operador);
+
+            return retorno;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        /// <summary>
+        /// limpia los textbox, el label de resultado y el combobox en blanco
+        /// </summary>
+        private void limpiar()
+        {
+            this.cmbOperador.SelectedIndex = 0;
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.lblResultado.Text = "";
+        }
+
+        /// <summary>
+        /// pregunta si esta seguro de cerrar la calculadora y la cierra
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("¿Esta seguro que quiere cerrar la calculadora?", "Cerrar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Numero binario = new Numero();
+            if (!string.IsNullOrEmpty(this.lblResultado.Text))
+                this.lblResultado.Text = binario.DecimalBinario(this.lblResultado.Text);
+            else
+                this.lblResultado.Text = "Valor invalido";
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero nDecimal = new Numero();
+            if (!string.IsNullOrEmpty(this.lblResultado.Text) )
+                this.lblResultado.Text = nDecimal.BinarioDecimal(this.lblResultado.Text);
+            else
+                this.lblResultado.Text = "Valor invalido";
+        }
+    }
+}
